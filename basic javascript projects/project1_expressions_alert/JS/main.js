2be3a30cdf42f8ab990c1353bf7fd527954a508e
Gerez16 <117327280+Gerez16@users.noreply.github.com>
2023-02-13 21:43:58 -0800

    Add files via upload
    added on 2/13/23

---
+++ basic javascript projects/project1_expressions_alert/JS/main.js
@@ -0,0 +1,3 @@
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", //defining a variable
+    Daughter = "Penny", Son = "Zoro";
+document.write(Dad); // "dad" will appear for jeremiah
