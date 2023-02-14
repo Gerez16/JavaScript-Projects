e85e13a373d87f225a7d0c9985c2e3f2a951214c
Gerez16 <117327280+Gerez16@users.noreply.github.com>
2023-02-13 22:29:35 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var sent1 = "This is the begining of the string" // th
+    sent2 = "This is the end of the string"
+document.write("This is the begining of the string," // i utilized the concatenate ( linked both sentences together)
++ "This is the end of the string");
+
+window.alert("Welcome to the page!"); // this is my pop up grettings box
+
+document.write(3+3); // This would be my expression example and will show after the variables.
