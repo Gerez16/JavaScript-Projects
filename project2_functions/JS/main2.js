b46a0a0bd78bdb60542946ce3436849cadf92b98
Gerez16 <117327280+Gerez16@users.noreply.github.com>
2023-02-14 21:58:47 -0800

    Add files via upload

---
+++ project2_functions/JS/main2.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    var sentence = "My first car";
+    sentence += " and still one of my favorites was a  white infiniti g35 coupe";
+    document.getElementById("Concatenate") .innerHTML = sentence;
+}
