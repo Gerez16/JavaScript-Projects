3d3388533ced3aebcb44f59ec29596c7248f265b
Gerez16 <117327280+Gerez16@users.noreply.github.com>
2023-02-14 21:11:21 -0800

    Delete basic javascript projects/project1_expressions_alert directory

--- basic javascript projects/project1_expressions_alert/JS/main.js
+++
@@ -1,3 +0,0 @@
-var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", //defining a variable
-    Daughter = "Penny", Son = "Zoro";
-document.write(Dad); // "dad" will appear for jeremiah
