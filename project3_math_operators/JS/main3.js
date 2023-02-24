9e50b6c101e51d5a27bf6c77ed1abcd6476f83e8
Gerez16 <117327280+Gerez16@users.noreply.github.com>
2023-02-23 21:38:05 -0800

    Add files via upload

---
+++ project3_math_operators/JS/main3.js
@@ -0,0 +1,9 @@
+function multipliction() {
+    var simple_math = 6 * 8;
+    document.getElementById("math") .innerHTML = "6 x 8 =" + simple_math;
+}
+
+function division() {
+    var simple_math = 48/6;
+    document.getElementById("math").innerHTML = "48/6 = " + simple_math
+}
