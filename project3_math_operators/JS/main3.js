1652fc4fe5e06664fa629b389a17bbdd73db223b
Gerez16 <117327280+Gerez16@users.noreply.github.com>
2023-03-05 13:01:04 -0800

    Add files via upload

--- project3_math_operators/JS/main3.js
+++ project3_math_operators/JS/main3.js
@@ -1,12 +1,12 @@
-function multipliction() {      // defining a function and naming it
+function multiplication() {      // defining a function and naming it
     var simple_math = 6 * 8;    //  defining a variable and giving it
                                 
-    document.getElementById("math1") .innerHTML = "6 x 8 =" + simple_math;
+    document.getElementById("math") .innerHTML = "6 x 8 =" + simple_math;
 }
 
 function division() {       //defining a function and naming it
     var simple_math = 48/6; // defining a variable
-    document.getElementById("button_text").innerHTML = "48/6 = " + simple_math;
+    document.getElementById("math").innerHTML = "48/6 = " + simple_math;
 }
 
 function subtraction_function() {   //defining a function and namng it
@@ -29,7 +29,7 @@ document.write(x);
 
 function modulus_operator() {
     var simple_math = 25 % 6;
-    document.getElementById("math").innerHTML = "when you divide 25 by 6 you have a
+    document.getElementById("math").innerHTML = "when you divide 25 by 6 you have a \
     remainder of: " + simple_math;
 }
-window.alert(math.random() *100);
+window.alert(Math.random() *100);
