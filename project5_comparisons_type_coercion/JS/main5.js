14024476bf310ac5f17158b6f657c8f6fcdf0610
Gerez16 <16gerardoalvarez@gmail.com>
2023-03-08 12:24:37 -0800

    changes made

--- project5_comparisons_type_coercion/JS/main5.js
+++ project5_comparisons_type_coercion/JS/main5.js
@@ -1,2 +1,20 @@
-document.write(10==10);
-document.write(3==11);
+var Days = "favorite weekday", monday = "least favorite", friday = "the favorite"; // createad variables
+document.write(typeof friday);      // utilizing the document.write method
+
+document.write ("\"In the month of december," // combining a string and number also by concatenate and using the + operator
++ "their are "+31+" days in that month.");
+
+document.write(5>2 && 10> 4);
+document.write(10 > 20 || 12 > 4);
+document.write( 10 == 12); // using the double equal sign to show that a comparison should be made.
+
+z = 100;
+a = 100;
+document.write(z === a); // using the triple equal sign for a comparison 
+
+function not_Function() {       // defining a function and naming it
+    document.getElementById("Not").innerHTML = ! (10 > 20);
+}
+
+document.write(110 > 80);
+document.write(20 < 3 );
