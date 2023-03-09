eecf15cc7c95b8fb3c3bd892401136eb1b4b2cc8
Gerez16 <16gerardoalvarez@gmail.com>
2023-03-08 20:56:13 -0800

    ..

---
+++ project6_ternary__operators_constructions/JS/main6.js
@@ -0,0 +1,25 @@
+function Ride_Function() {
+    var Height, Can_ride;
+    Height = document.getElementById("Height").value;
+    Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
+    document.getElementById("Ride").innerHTML = Can_ride + " to ride";
+}
+
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge","Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction() {
+    document.getElementById("Keyword_and_Constructors").innerHTML =
+    "Erik drives a&nbsp"  + Erik.Vehicle_Color + "-colored&nbsp" + Erik.Vehicle_Model + "&nbsp manufactured in " + Erik.Vehicle_Year
+    
+}
+
+document.getElementById("New_and_This").innerHTML
+
+document.getElementById("Nested_Function").innerHTML
