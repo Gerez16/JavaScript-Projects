9ddbc38598dd3a1d603cdd4c32773511f9f3387f
Gerez16 <117327280+Gerez16@users.noreply.github.com>
2023-03-12 15:52:18 -0700

    Add files via upload

--- project6_ternary__operators_constructions/JS/main6.js
+++ project6_ternary__operators_constructions/JS/main6.js
@@ -1,12 +1,15 @@
-function Ride_Function() {
-    var Height, Can_ride;
+function Ride_Function() {  // defining a function and naiming it
+    var Height, Can_ride;   //defining a variable and giving it
+                            // a string value
     Height = document.getElementById("Height").value;
     Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
-    document.getElementById("Ride").innerHTML = Can_ride + " to ride";
+    document.getElementById("Ride").innerHTML = Can_ride + " to ride";  //putting the value 
+
+                                                                        //of the variable intp the HTML elementfrompoint
 }
 
-function Vehicle(Make, Model, Year, Color) {
-    this.Vehicle_Make = Make;
+function Vehicle(Make, Model, Year, Color) {   //chose a function
+    this.Vehicle_Make = Make;   
     this.Vehicle_Model = Model;
     this.Vehicle_Year = Year;
     this.Vehicle_Color = Color;
@@ -20,6 +23,15 @@ function myFunction() {
     
 }
 
-document.getElementById("New_and_This").innerHTML
+document.getElementById("New_and_This").innerHTML //
 
-document.getElementById("Nested_Function").innerHTML
+function count_Function() {
+    document.getElementById(Nested_Function).innerHTML = count();
+    function count() {
+        var starting_point= 9;
+        function Plus_one() {Starting_point += 1;}
+        Plus_one();
+        return Starting_point;
+    }
+}
+
