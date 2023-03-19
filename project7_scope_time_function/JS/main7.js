f3b400dc0dc29cc21a1aaf7a9cea3ac74ad71dd5
Gerez16 <117327280+Gerez16@users.noreply.github.com>
2023-03-19 15:28:17 -0700

    Add files via upload

---
+++ project7_scope_time_function/JS/main7.js
@@ -0,0 +1,49 @@
+function get_Date() {
+    if (new Date().getHours()  < 10) {
+    document.getElementById("Welcome").innerHTML = "Please call before our closing time";
+    }
+}
+
+if ( 200 > 100) {
+    document.write(" The right number is smaller than the number on the right.")
+
+}
+
+function Height_Function() {
+    Height = document.getElementById("Height").value;
+    if (Height >= 6 foot) {
+        Ride = "You are tall enough to get on the adult rides";
+    }
+    else {
+        Ride = "You are not tall enough to get on the rides";
+    }
+    document.getElementById("Height").innerHTML = Height;
+}
+
+var x = 300;
+function Add_numbers_1() {
+    document.write(10 + x + "<br>");
+   
+}
+function Add_numbers_2() {
+    document.write(x + 50);
+}
+    
+Add_numbers_1();
+Add_numbers_2();
+
+function Add_numbers_1() {
+    var x = 20;
+    document.write(30 + x );
+}
+function Add_numbers_2() {
+    document.write(x + 100);
+}
+Add_numbers_1;
+Add_numbers_2;
+
+console.log(20 + 20);
+document.getElementById("plus").innerHTML
+
+
+
