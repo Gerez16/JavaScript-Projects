eb2dd50223c1ab366f38f1732fbcf5787c2bb909
Gerez16 <117327280+Gerez16@users.noreply.github.com>
2023-03-23 14:22:47 -0700

    Add files via upload

--- project7_scope_time_function/JS/main7.js
+++ project7_scope_time_function/JS/main7.js
@@ -11,39 +11,35 @@ if ( 200 > 100) {
 
 function Height_Function() {
     Height = document.getElementById("Height").value;
-    if (Height >= 6 foot) {
+    if (Height >= 6) {
         Ride = "You are tall enough to get on the adult rides";
     }
     else {
         Ride = "You are not tall enough to get on the rides";
     }
-    document.getElementById("Height").innerHTML = Height;
+    document.getElementById("This").innerHTML = Ride;
 }
 
-var x = 300;
-function Add_numbers_1() {
-    document.write(10 + x + "<br>");
-   
+var X = 100;
+function Add_numbers_1()  {
+    document.write(10 + X + "<br>");
 }
 function Add_numbers_2() {
-    document.write(x + 50);
+    document.write(X + 100);
 }
-    
 Add_numbers_1();
 Add_numbers_2();
 
-function Add_numbers_1() {
-    var x = 20;
-    document.write(30 + x );
+function Add_numbers_3() {
+    var X = 50;
+    document.write(30 + X + "<br>");
 }
-function Add_numbers_2() {
-    document.write(x + 100);
+function Add_numbers_4() {
+    document.write(X + 100);
 }
-Add_numbers_1;
-Add_numbers_2;
-
-console.log(20 + 20);
-document.getElementById("plus").innerHTML
+Add_numbers_3();
+Add_numbers_4();
 
 
-
+console.log(20 + 20); // this should be viewd on developer tools
+document.getElementById("plus").innerHTML // the ID is voided
